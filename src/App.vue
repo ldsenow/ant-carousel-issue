8da025774df80e0be49dd44e03c86b190a45223f
ldsenow <ldsenow@gmail.com>
2022-05-13 11:58:26 +1000

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import { Carousel } from "ant-design-vue";
+</script>
+
+<template>
+  <Carousel>
+    <HelloWorld msg="Test A" />
+    <HelloWorld msg="Test B" />
+  </Carousel>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, onUnmounted } from "vue";
+
+const props = defineProps<{ msg: string }>();
+
+onMounted(() => {
+  console.log(`onMounted: ${props.msg}`);
+});
+
+onBeforeUnmount(() => {
+  console.log(`onBeforeUnmount: ${props.msg}`);
+});
+
+onUnmounted(() => {
+  console.log(`onUnmounted: ${props.msg}`);
+});
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
